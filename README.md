97e6687863c1551eea404e499dc04fc3729159b2
ibmdev75 <ibmdev75@gmail.com>
2014-07-21 00:12:30 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 cloudbees-clickstart-spring-angularjs-jboss
 ===========================================
+Url Local : http://localhost:8080/spring-app-web
---
+++ spring-app-web/src/test/java/com/ibm/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.ibm;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class HelloWorld extends TestCase {
+
+	@Test
+	public void test() {
+		System.out.println("Chapitre 1 : Hello World Test Case");
+	}
+
+}
